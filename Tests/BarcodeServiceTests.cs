14ed8e910b52dc8a0e9a68059b0119a9eb13091d
Lucas Messias <lucasfmessias@gmail.com>
2026-04-10 22:25:36 -0300

    feat: refatoração do pipeline CI/CD com cobertura de testes

--- Tests/BarcodeServiceTests.cs
+++ Tests/BarcodeServiceTests.cs
@@ -131,6 +131,42 @@ public class BarcodeServiceTests
     }
 
     [Test]
+    public void Generate_ShouldThrowException_WhenAnyRequiredFieldIsEmpty()
+    {
+        var service = new BarcodeService();
+
+        // Testando um cenário onde o SupplierCode está vazio
+        var input = new BarcodeInput
+        {
+            SupplierCode = "", // Vazio aciona o primeiro IF
+            PartNumber = "96510BP100",
+            SequenceCode = "DCU1",
+            TraceabilityCode = "260128AD11"
+        };
+
+        var ex = Assert.Throws<ArgumentException>(() => service.Generate(input));
+        Assert.That(ex.Message, Is.EqualTo("One or more required fields are null or whitespace."));
+    }
+
+    [TestCase(null, "96510BP100", "DCU1", "260128AD11ABRL35883037602096000021")]
+    [TestCase("ANFU", null, "DCU1", "260128AD11ABRL35883037602096000021")]
+    [TestCase("ANFU", "96510BP100", null, "260128AD11ABRL35883037602096000021")]
+    [TestCase("ANFU", "96510BP100", "DCU1", null)]
+    public void Generate_ShouldThrowException_ForEachNullField(string? v, string? partNo, string? alc, string? traceab)
+    {
+        var service = new BarcodeService();
+        var input = new BarcodeInput 
+        { 
+            SupplierCode = v, 
+            PartNumber = partNo, 
+            SequenceCode = alc, 
+            TraceabilityCode = traceab
+        };
+
+        Assert.Throws<ArgumentException>(() => service.Generate(input));
+    }
+
+    [Test]
     public void Should_Generate_Complete_Code()
     {
         var service = new BarcodeService();
--- Tests/obj/Debug/net10.0/Tests.AssemblyInfo.cs
+++ Tests/obj/Debug/net10.0/Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d99bfecfb24e9071ad9eae8e68a2a6c6e22c2f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2c35ed10c0f6634d0d164eeb97562a8767bb806")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
